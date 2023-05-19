42e2b829d94cc207aaac981581c96b870460eb04
biter0o1 <biter0o1@o2.pl>
2023-05-19 16:35:07 +0200

    Fix db path, add dog list, update form

--- hello.py
+++ hello.py
@@ -1,27 +1,43 @@
 import os
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField, FileField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
-current_path = os.path.abspath(__file__)
+
+current_path = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(current_path, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    return render_template('index.html', dogs = Dog.query.all())
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        session['name'] = form.name.data
+        dog = Dog(
+            name = request.form.get('name'),
+            breed = request.form.get('breed'),
+            age = request.form.get('age'),
+            photo = request.form.get('photo'))
+
+        db.session.add(dog)
+        db.session.commit()
+        
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('create.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -35,7 +51,10 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('Imie', validators=[DataRequired()])
+    breed = StringField('Rasa', validators=[DataRequired()])
+    age = IntegerField('Wiek', validators=[DataRequired()])
+    photo = FileField('Zdjecie')
     submit = SubmitField('Submit')
 
 
@@ -65,7 +84,7 @@ class User(db.Model):
     
 
 class Dog(db.Model):
-    __tablename__ = 'dog',
+    __tablename__ = 'dog'
     id = db.Column(db.Integer,
         primary_key=True)
     name = db.Column(db.String(64),

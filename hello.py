b5718aeb7d8f8b8b08b14566959a9f014bfb8275
Radek011200 <beziek120@o2.pl>
2023-05-21 16:14:27 +0200

    fix errors

--- hello.py
+++ hello.py
@@ -99,7 +99,9 @@ def dog_delete(id):
 
 @app.errorhandler(404)
 def page_not_found(e):
+
     return render_template('404.html'), 404
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
@@ -153,3 +155,6 @@ class Dog(db.Model):
         self.age = age
         self.breed = breed
         self.photo = photo
+
+if __name__ == '__main__':
+    app.run()
